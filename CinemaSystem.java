2af78924a4ede25faff5501c6e16a2b7bbe63edd
Joseph Jensen <jensenj.mh@gmail.com>
2017-10-21 14:56:25 +0100

    Indentation fix
    Fixed indentation on file

--- CinemaSystem.java
+++ CinemaSystem.java
@@ -12,47 +12,47 @@ public class CinemaSystem {
 		//Take requested ticket amounts from stdin
 		//Keep tally of total tickets and total cost
 		try {
-		System.out.println("How many Standard tickets do you wish to purchase?");
-		ticketAmount = sc.nextInt();
-		if (ticketAmount < 0)
-			throw new IllegalArgumentException("This number cannot be negative!");
-		totalTickets += ticketAmount;
-		totalCost = new StandardTicket().getCost() * ticketAmount;
+			System.out.println("How many Standard tickets do you wish to purchase?");
+			ticketAmount = sc.nextInt();
+			if (ticketAmount < 0)
+				throw new IllegalArgumentException("This number cannot be negative!");
+			totalTickets += ticketAmount;
+			totalCost = new StandardTicket().getCost() * ticketAmount;
 		
-		System.out.println("How many OAP tickets do you wish to purchase?");
-		ticketAmount = sc.nextInt();
-		if (ticketAmount < 0)
-			throw new IllegalArgumentException("This number cannot be negative!");
-		totalTickets += ticketAmount;
-		totalCost += new OAPTicket().getCost() * ticketAmount;
+			System.out.println("How many OAP tickets do you wish to purchase?");
+			ticketAmount = sc.nextInt();
+			if (ticketAmount < 0)
+				throw new IllegalArgumentException("This number cannot be negative!");
+			totalTickets += ticketAmount;
+			totalCost += new OAPTicket().getCost() * ticketAmount;
 		
-		System.out.println("How many Student tickets do you wish to purchase?");
-		ticketAmount = sc.nextInt();
-		if (ticketAmount < 0)
-			throw new IllegalArgumentException("This number cannot be negative!");
-		totalTickets += ticketAmount;
-		totalCost += new StudentTicket().getCost() * ticketAmount;
+			System.out.println("How many Student tickets do you wish to purchase?");
+			ticketAmount = sc.nextInt();
+			if (ticketAmount < 0)
+				throw new IllegalArgumentException("This number cannot be negative!");
+			totalTickets += ticketAmount;
+			totalCost += new StudentTicket().getCost() * ticketAmount;
 				
-		System.out.println("How many Child tickets do you wish to purchase?");
-		ticketAmount = sc.nextInt();
-		if (ticketAmount < 0)
-			throw new IllegalArgumentException("This number cannot be negative!");
-		totalTickets += ticketAmount;
-		totalCost += new ChildTicket().getCost() * ticketAmount;
+			System.out.println("How many Child tickets do you wish to purchase?");
+			ticketAmount = sc.nextInt();
+			if (ticketAmount < 0)
+				throw new IllegalArgumentException("This number cannot be negative!");
+			totalTickets += ticketAmount;
+			totalCost += new ChildTicket().getCost() * ticketAmount;
 		
-		//Check if today is Wednesday. If so, apply discount
-		Date now = new Date();
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E");
-		if (simpleDateFormat.format(now).equals("Wed"))
-			totalCost -= (2 * totalTickets);
+			//Check if today is Wednesday. If so, apply discount
+			Date now = new Date();
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E");
+			if (simpleDateFormat.format(now).equals("Wed"))
+				totalCost -= (2 * totalTickets);
 		
-		System.out.println("------------------------------------------------");
-		System.out.println("Total price of your tickets is: \u00A3" + totalCost);
-		} //try
+			System.out.println("------------------------------------------------");
+			System.out.println("Total price of your tickets is: \u00A3" + totalCost);
+			} //try
 		//If user enters non-integer
-		catch (InputMismatchException e) {
-			System.out.println("Please only enter integers greater than or equal to zero!");
-			System.out.println("Terminating...");
-		} //catch
+			catch (InputMismatchException e) {
+				System.out.println("Please only enter integers greater than or equal to zero!");
+				System.out.println("Terminating...");
+			} //catch
 	} //main()
 }
